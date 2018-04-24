6eb42ef70a780d4d2b1565dd4f8b4a99d28ee901
Justin Lazarski <blackhawks189@gmail.com>
2018-04-24 14:19:57 -0700

    Finished the iOSCreator Tutorial

--- iOS11SliderTutorial/ViewController.swift
+++ iOS11SliderTutorial/ViewController.swift
@@ -11,14 +11,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var label: UILabel!
+    
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        let currentValue = Int(sender.value)
+        label.text = "\(currentValue)"
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
